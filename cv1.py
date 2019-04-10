cd5e732f4caff266b7ef6d0850e60ae16500ab1d
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2019-04-10 11:50:38 +0900

    크롤링한거 한번 돌려봄

--- cv1.py
+++ cv1.py
@@ -1,89 +1,41 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('./5.jpeg')
+image = cv2.imread('./croll/croll3.jpg',cv2.IMREAD_COLOR)
 
 row,col,_ = image.shape
-
+print(image.shape)
 row = int(row/2)
 col = int(col/2)
-image = cv2.resize(image,(col,row))
+print(row,col)
+image = image[47:374,:col]
 
 cv2.imshow('image',image)
+grayimage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-hsvimage = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-h,s,v = cv2.split(hsvimage)
+# cv2.imshow('gray',grayimage)
 
+sobelX = cv2.Sobel(image, cv2.CV_64F, 1, 0)
+sobelY = cv2.Sobel(image, cv2.CV_64F, 0, 1)
 
-z = np.zeros((row,col))
-for i in range(row):
-    for j in range(col):
-         z[i][j] = v[i][j] / 255
+sobelX = np.uint8(np.absolute(sobelX))
+sobelY = np.uint8(np.absolute(sobelY))
+add = sobelY + sobelX
 
-b3 = np.zeros((row,col))
-b5 = np.zeros((row,col))
-b3_1 = np.zeros((row,col))
+cv2.imshow('add',add)
+ret, th1 = cv2.threshold(add,127,255,cv2.THRESH_BINARY)
 
-for i in range(row):
-    for j in range(col):
-        if z[i][j] < 0.3:
-            b3[i][j] = 1.0
-        else:
-            b3[i][j] = 0.0
+th3 = cv2.adaptiveThreshold(add,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+cv2.THRESH_BINARY,15,2)
+cv2.imshow('th1',th1)
 
-# cv2.imshow('b3',b3)
-for i in range(row):
-    for j in range(col):
-        if z[i][j] >= 0.3 and z[i][j] < 0.5:
-            b5[i][j] = 1.0
-        else:
-            b5[i][j] = 0.0
-# cv2.imshow('b5',b5)
-
-for i in range(row):
-    for j in range(col):
-        b3_1[i][j] = 1 - b3[i][j]
-# cv2.imshow('b3_1',b3_1)
-
-m3 = 0.0
-for i in range(row):
-    for j in range(col):
-        m3 = m3 + b3[i][j]*z[i][j]
+cv2.imshow('th3',th3)
 
 
-m3_1 = 0.0
-for i in range(row):
-    for j in range(col):
-        m3_1 = m3_1 + b3[i][j]
+# edges = cv2.Canny(image,50,200)
+
+# cv2.imshow('canny',edges)
 
 
-m3 = m3/m3_1
-print('m3',m3)
-
-bw = np.zeros((row,col))
-for i in range(row):
-    for j in range(col):
-        bw[i][j] = b3[i][j] + 0.7*b5[i][j]
-# cv2.imshow('bw',bw)
-
-g_v = cv2.GaussianBlur(z,(3,3),5)
-
-# cv2.imshow('g_v',g_v)
-
-g_bw = cv2.GaussianBlur(bw,(31,31),5)
-# cv2.imshow('g_bw',g_bw)
-
-for i in range(row):
-    for j in range(col):
-        z[i][j] = z[i][j] + g_v[i][j]*g_bw[i][j]
-
-cv2.imshow('z',z)
-print(z)
-
-hsv = cv2.merge([h,s,z])
-
-# print(hsv.shape)
-# image = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-# cv2.imshow('image1',image)
 
 cv2.waitKey(0)
