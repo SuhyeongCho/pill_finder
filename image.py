c53e862e5346d8a94aebd3707e2eef2bdf86a247
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2019-05-24 11:00:58 +0900

    글자 이미지 프로세싱 파싱 파일 완료

--- image.py
+++ image.py
@@ -5,12 +5,12 @@ import os
 from image_processing import *
 from findtext import *
 
-# directory = './crawling/shape_01'
-# extension = '.jpg'
-for i in range(1,10000):
-    # file_index = directory + '/_' + str(i)
-    # file_path = file_index + extension
+s_cnt = 31750
+
+for i in range(1,9000):
     file_path = '/Users/suhyeongcho/Desktop/crawling/shape_01/'+str(i)+'.jpg'
+    shape_path = '/Users/suhyeongcho/Desktop/SHAPE/'
+    thres_path = '/Users/suhyeongcho/Desktop/THRES/'
     try:
         img = cv2.imread(file_path,cv2.IMREAD_COLOR)
         row,col,_ = img.shape
@@ -18,38 +18,29 @@ for i in range(1,10000):
         img_l = img[:,:390]
         img_r = img[:,390:]
     except:
-        break
+        print('Cannot found',str(i)+'.jpg')
+        continue
     try:
-        cv2.imshow('imgl',img_l)
         imgcolorc,imgcolorf,_,_ = image_processing(img_l)
         # img = cv2.resize(img,(96,96))
         print(file_path,"left is processed")
         origarr, textarr = findtext(imgcolorc,imgcolorf)
-        for happy1,happy2 in zip(origarr,textarr):
-            cv2.imshow('happy1',happy1)
-            cv2.imshow('happy2',happy2)
-
-            cv2.waitKey(0)
-        # cv2.imwrite(file_index + '_l' + extension,img)
+        for original,text in zip(origarr,textarr):
+            cv2.imwrite(shape_path+str(s_cnt)+'.jpg',original)
+            cv2.imwrite(thres_path+str(s_cnt)+'.jpg',text)
+            s_cnt = s_cnt + 1
     except:
             print("Error Occcured! at",file_path,"left")
     try:
-        cv2.imshow('imgr',img_r)
-        imgcolorc,imgcolorf,_,_ = image_processing(img_r)
+        imgcolorc,imgcolorf,_,_ = image_processing(img_l)
         # img = cv2.resize(img,(96,96))
         print(file_path,"right is processed")
         origarr, textarr = findtext(imgcolorc,imgcolorf)
-        for happy1,happy2 in zip(origarr,textarr):
-            cv2.imshow('happy4',happy1)
-            cv2.imshow('happy5',happy2)
-
-            cv2.waitKey(0)
-
-        # cv2.imwrite(file_index + '_r' + extension,img)
-
+        for original,text in zip(origarr,textarr):
+            cv2.imwrite(shape_path+str(s_cnt)+'.jpg',original)
+            cv2.imwrite(thres_path+str(s_cnt)+'.jpg',text)
+            s_cnt = s_cnt + 1
     except:
-        print("Error Occcured! at",file_path,"right")
+            print("Error Occcured! at",file_path,"right")
 
     # os.remove(file_path)
-    
-    
