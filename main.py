1c6d49296897b4aa71890fe3a206b575bd130757
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2019-06-05 06:17:14 +0900

    인코딩된 라벨링을 디코딩

--- main.py
+++ main.py
@@ -5,13 +5,42 @@ from image_processing import image_processing
 from make_square import make_square
 from findtext import findtext
 
+def char_decoding(c):
+    if c < 10:
+        return str(chr(c+48))
+    else:
+        return str(chr(c - 10 + 65))
+
+def shape_decoding(s):
+    if s == 0:
+        return "원형"
+    elif s == 1:
+        return "타원형"
+    elif s == 2:
+        return "삼각형"
+    elif s == 3:
+        return "사각형"
+    elif s == 4:
+        return "마름모형"
+    elif s == 5:
+        return "오각형"
+    elif s == 6:
+        return "육각형"
+    elif s== 7:
+        return "팔각형"
+
 image = cv2.imread('test1.jpeg')
 imgcolorc,imgcolorf,imgshape,colorname = image_processing(image)
 _,thres = findtext(imgcolorc,imgcolorf)
 
+print("char : ",end="")
 for th in thres:
     th_sq = make_square(th)
-    print(char_test(th_sq)[0])
+    c = char_test(th_sq)[0]
+    print(char_decoding(c),end="")
+print()
 
-print(shape_test(imgshape)[0])
-print(colorname)
+s = shape_test(imgshape)[0]
+print("shape :",shape_decoding(s))
+
+print("color :",colorname)
